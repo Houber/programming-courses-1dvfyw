1db30f74217e123a19bb33be63b68679047fbd1b
Houber <berradah2003@gmail.com>
2022-01-23 13:30:11 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,14 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
-let num: number;
-num = 12;
-num = num + 10;
-console.log(num);
+let N1, N2, SOUS: number;
+N1 = parseInt(prompt('Donner le premier nombre'));
+N2 = parseInt(prompt('doner le deuxieme nombre'));
+
+if (N1 > N2) {
+  SOUS = N1 - N2;
+} else {
+  SOUS = N2 - N1;
+}
+
+console.log(SOUS);
